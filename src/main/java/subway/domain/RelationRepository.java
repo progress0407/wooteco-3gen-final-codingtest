6a7b91c8c21fb418d1095fa6f6b9e523887f3fdd
swcho <progress0407@gmail.com>
2021-11-28 14:54:57 +0900

    feat(RelationRepository): 거리가중치, 시간 가중치 재설정

--- src/main/java/subway/domain/RelationRepository.java
+++ src/main/java/subway/domain/RelationRepository.java
@@ -10,11 +10,11 @@ public class RelationRepository {
 
 	private static WeightedMultigraph<String, DefaultWeightedEdge> distanceRelation = new WeightedMultigraph<>(
 		DefaultWeightedEdge.class);
-
 	private static WeightedMultigraph<String, DefaultWeightedEdge> timeRelation = new WeightedMultigraph<>(
 		DefaultWeightedEdge.class);
 
-	private static DijkstraShortestPath dijkstraShortestPath;
+	private static DijkstraShortestPath dijkstraShortestDistance;
+	private static DijkstraShortestPath dijkstraShortestTime;
 
 	static {
 		initData();
@@ -23,20 +23,43 @@ public class RelationRepository {
 	private static void initData() {
 		addVertex();
 		setDistanceEdgeWeight();
-		dijkstraShortestPath = new DijkstraShortestPath(distanceRelation);
+		setTimeEdgeWeight();
 	}
 
 	private static void setDistanceEdgeWeight() {
 		// 2호선
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("교대역", "강남역"), 2);
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("강남역", "역삼역"), 3);
+		addEdgeWeight("교대역", "강남역", 2);
+		addEdgeWeight("강남역", "역삼역", 3);
 		// 3호선
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("교대역", "남부터미널역"), 3);
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("남부터미널역", "양재역"), 6);
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("양재역", "매봉역"), 1);
+		addEdgeWeight("교대역", "남부터미널역", 3);
+		addEdgeWeight("남부터미널역", "양재역", 6);
+		addEdgeWeight("양재역", "매봉역", 1);
 		// 신분당선
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("강남역", "양재역"), 2);
-		distanceRelation.setEdgeWeight(distanceRelation.addEdge("양재역", "양재시민의숲역"), 10);
+		addEdgeWeight("강남역", "양재역", 2);
+		addEdgeWeight("양재역", "양재시민의숲역", 10);
+		dijkstraShortestDistance = new DijkstraShortestPath(distanceRelation);
+	}
+
+	private static void setTimeEdgeWeight() {
+		// 2호선
+		addTimeEdgeWeight("교대역", "강남역", 3);
+		addTimeEdgeWeight("강남역", "역삼역", 3);
+		// 3호선
+		addTimeEdgeWeight("교대역", "남부터미널역", 2);
+		addTimeEdgeWeight("남부터미널역", "양재역", 5);
+		addTimeEdgeWeight("양재역", "매봉역", 1);
+		// 신분당선
+		addTimeEdgeWeight("강남역", "양재역", 8);
+		addTimeEdgeWeight("양재역", "양재시민의숲역", 3);
+		dijkstraShortestTime = new DijkstraShortestPath(timeRelation);
+	}
+
+	private static void addTimeEdgeWeight(String sourceStation, String destinationStation, int timeWeight) {
+		timeRelation.setEdgeWeight(timeRelation.addEdge(sourceStation, destinationStation), timeWeight);
+	}
+
+	private static void addEdgeWeight(String sourceStation, String destinationStation, int distanceWeight) {
+		distanceRelation.setEdgeWeight(distanceRelation.addEdge(sourceStation, destinationStation), distanceWeight);
 	}
 
 	private static void addVertex() {
@@ -47,8 +70,13 @@ public class RelationRepository {
 		});
 	}
 
-	public static int getShortestPath(String source, String destination) {
-		double weight = dijkstraShortestPath.getPath(source, destination).getWeight();
+	public static int getShortestDistance(String source, String destination) {
+		double weight = dijkstraShortestDistance.getPath(source, destination).getWeight();
+		return (int) weight;
+	}
+
+	public static int getShortestTime(String source, String destination) {
+		double weight = dijkstraShortestTime.getPath(source, destination).getWeight();
 		return (int) weight;
 	}
 }
